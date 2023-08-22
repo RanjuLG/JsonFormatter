19adc77e3be04b54945652a136a175a2b2986d43
RanjuLG <ranjue16@gmail.com>
2023-08-22 16:18:11 +0530

    First Commit

---
+++ JsonFormatter/Class1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonFormatter
+{
+    public class ExtensionUnit
+    {
+        public int Id { get; set; }
+        public string ExtensionId { get; set; }
+        public string UnitId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+
+
+    public class ExtensionList
+    {
+        public int Id { get; set; }
+        public string ExtensionId { get; set; }
+        public string RequestType { get; set; }
+        public string ParentExtensionId { get; set; }
+        public DateTime Date { get; set; }
+        public string DisplayDate { get; set; }
+        public string StartTime { get; set; }
+        public string DisplayStartTime { get; set; }
+        public string EndTime { get; set; }
+        public string DisplayEndTime { get; set; }
+        public string ExtensionType { get; set; }
+        public string DisplayExtensionType { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public bool TmApproval { get; set; }
+        public bool FmApproval { get; set; }
+        public bool HasEditRequest { get; set; }
+        public bool HasTerminateRequest { get; set; }
+        public int RequestBy { get; set; }
+        public int UpdatedBy { get; set; }
+        public string ApprovedBy { get; set; }
+        public string ApprovedAt { get; set; }
+        public string DisplayApprovedAt { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public List<ExtensionUnit> ExtensionUnits { get; set; }
+        public string Extension { get; set; }
+        public string ActivityLogs { get; set; }
+        public string DisplayCreatedAt { get; set; }
+        public string DisplayUpdatedAt { get; set; }
+        public string NewDescription { get; set; }
+        public List<object> StandingOrderExtensions { get; set; }
+    }
+
+    public class RootObject
+    {
+
+        public int DataCount { get; set; }
+        public List<ExtensionList> ExtensionList { get; set; }
+        public string SelectedExtension { get; set; }
+        public string ValidationError { get; set; }
+        public bool IsValidated { get; set; }
+        public bool IsSucceded { get; set; }
+        public string ExtensionId { get; set; }
+        public bool HavePendingEditRequests { get; set; }
+        public bool HavePendingTerminateRequests { get; set; }
+        public string ExtensionStatus { get; set; }
+        public string ExtensionType { get; set; }
+        public bool Authorized { get; set; }
+    }
+}
---
+++ JsonFormatter/Program.cs
@@ -0,0 +1,50 @@
+﻿using JsonFormatter;
+using System;
+using System.Diagnostics;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Json;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace HTTPClient;
+
+class Program
+{
+    static async Task Main(string[] args)
+
+    {
+        var url = "http://10.20.30.198:5001/api/extension?statusList=%5B%5D&requestType=%5B%22Select%20All%22%5D&page=1&pageSize=5&lstUnits=%5B%5D&startDate=22-Aug-2022&endDate=22-Aug-2024";
+
+        using var client = new HttpClient();
+
+        var result = await client.GetAsync(url);
+
+        try
+        {
+
+            var content = await client.GetStringAsync(url);
+
+            Console.WriteLine("Status Code: " + (int)result.StatusCode);
+            Console.WriteLine("Response Message: " + result.StatusCode);
+            //Console.WriteLine("Complete Header Response: " + result);
+            //.WriteLine("Content: " + content);
+            Console.Read();
+
+            var person = JsonSerializer.Deserialize<RootObject>(content);
+            Console.WriteLine(content);
+
+
+
+        }
+        catch (Exception)
+        {
+
+            Console.WriteLine("Status Code: " + (int)result.StatusCode);
+            Console.WriteLine("Response Message: " + result.StatusCode);
+        }
+        
+
+
+    }
+}
